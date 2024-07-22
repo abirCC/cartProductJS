440405be28a9c7c389bb69fff49d73825450abdb
abirCC <abir.banerjee@codeclouds.in>
2024-07-22 16:31:16 +0530

    initial changes

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let item = {
+  item_image: 'images/1.jpg',
+  rating: {
+    stars:4.5,
+    noOfReviews:1400,
+  },
+  company_name:'Carlton London',
+  item_name:'Rhodium-plated CZ Floral Studs',
+  current_price: 606,
+  original_price: 1045,
+  discount_percentage: 42,
+}
+let itemsContainerElement = document.querySelector('.items-container');
+itemsContainerElement.innerHTML = `
+          <div class="item-container">
+          <img class="item-image" src="${item.item_image}" alt="item image">
+          <div class="rating">${item.rating.stars}⭐| ${item.rating.noOfReviews}</div>
+          <div class="company-name">${item.company_name}</div>
+          <div class="item-name">${item.item_name}</div>
+          <div class="price">
+            <span class="current-price">Rs ${item.current_price}</span>
+            <span class="original-price">Rs ${item.original_price}</span>
+            <span class="discount">(${item.discount_percentage}% OFF)</span>
+          </div>
+          <button class="btn-add-bag">Add To Bag</button>
+         </div>`;
