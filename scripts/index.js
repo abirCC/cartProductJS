527c5f3e82962103aadd8590a7d144896f6f95db
abirCC <abir.banerjee@codeclouds.in>
2024-07-29 13:50:22 +0530

    final changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,46 @@
-let item = {
-  item_image: 'images/1.jpg',
-  rating: {
-    stars:4.5,
-    noOfReviews:1400,
-  },
-  company_name:'Carlton London',
-  item_name:'Rhodium-plated CZ Floral Studs',
-  current_price: 606,
-  original_price: 1045,
-  discount_percentage: 42,
+let bagItems;
+onLoad();
+
+function onLoad(){
+  let bagItemStr = localStorage.getItem('bagItems');
+  bagItems = bagItemStr ? JSON.parse(bagItemStr) : [];
+  displayItemsOnHomePage();
+  displayBagIcon();
 }
+
+function addToBag(itemId){
+  bagItems.push(itemId);
+  localStorage.setItem('bagItems',JSON.stringify(bagItems));
+  displayBagIcon();
+}
+function displayBagIcon(){
+  let bagItemCountElement = document.querySelector('.bag-item-count');
+  if (bagItems.length > 0) {
+    bagItemCountElement.style.visibility = 'visible';
+    bagItemCountElement.innerHTML = bagItems.length;
+  } else {
+    bagItemCountElement.style.visibility = 'hidden';
+  } 
+}
+function displayItemsOnHomePage(){
 let itemsContainerElement = document.querySelector('.items-container');
-itemsContainerElement.innerHTML = `
-          <div class="item-container">
-          <img class="item-image" src="${item.item_image}" alt="item image">
-          <div class="rating">${item.rating.stars}⭐| ${item.rating.noOfReviews}</div>
-          <div class="company-name">${item.company_name}</div>
-          <div class="item-name">${item.item_name}</div>
-          <div class="price">
-            <span class="current-price">Rs ${item.current_price}</span>
-            <span class="original-price">Rs ${item.original_price}</span>
-            <span class="discount">(${item.discount_percentage}% OFF)</span>
-          </div>
-          <button class="btn-add-bag">Add To Bag</button>
-         </div>`;
+let innerHtml = '';
+if(!itemsContainerElement){
+  return;
+}
+items.forEach(function(item){
+innerHtml +=`<div class="item-container">
+<img class="item-image" src="${item.image}" alt="item image">
+<div class="rating">${item.rating.stars}⭐| ${item.rating.count}</div>
+<div class="company-name">${item.company}</div>
+<div class="item-name">${item.item_name}</div>
+<div class="price">
+<span class="current-price">Rs ${item.current_price}</span>
+<span class="original-price">Rs ${item.original_price}</span>
+<span class="discount">(${item.discount_percentage}% OFF)</span>
+</div>
+<button class="btn-add-bag" onclick="addToBag(${item.id})">Add To Bag</button>
+</div>`
+});
+itemsContainerElement.innerHTML = innerHtml;
+}
